4cd2efc8b58c475e71d653744fcc6cc2eb5e3e0d
Andrew Costa <ancosta@vmware.com>
2021-08-25 10:47:24 -0700

    Add CFRoute and CFDomain resources (#15)
    Add CFRoute and CFDomain resources
    
    #13 
    
    Co-authored-by: Dave Walter <walterda@vmware.com>
    Co-authored-by: Akira Wong <wakira@vmware.com>
    Co-authored-by: Andrew Costa <ancosta@vmware.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -77,6 +77,12 @@ var _ = BeforeSuite(func() {
 	err = workloadsv1alpha1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
+	err = workloadsv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = workloadsv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
 	//+kubebuilder:scaffold:scheme
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
