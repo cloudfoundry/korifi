715dea1ecfef24bc5aaeead1f2a4ad8975b39988
Ashwin Krishna <krishnaas@vmware.com>
2021-09-14 11:44:39 -0700

    Update CFAppLabelKey string to match the CR label syntax

--- apis/workloads/v1alpha1/shared_types.go
+++ apis/workloads/v1alpha1/shared_types.go
@@ -4,7 +4,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-const CFAppLabelKey = "apps.cloudfoundry.org/cf-app-guid"
+const CFAppLabelKey = "workloads.cloudfoundry.org/cf-app-guid"
 
 // ResourceReference defines a reference to an instance of a resource in Kubernetes
 type ResourceReference struct { // TODO: replace this with k8s.io/api/core/v1.LocalObjectReference
