63dac69f7bc3264b4c2855102efe5f6016ab774c
Dave Walter <walterda@vmware.com>
2021-10-04 15:36:08 -0700

    Update the install-dependencies.sh script
    - make the GCP service account JSON file arg optional
    
    Co-authored-by: Andrew Costa <ancosta@vmware.com>
    Co-authored-by: Andrew Wittrock <awittrock@vmware.com>

--- hack/install-dependencies.sh
+++ hack/install-dependencies.sh
@@ -16,10 +16,6 @@ EOF
   exit 1
 }
 
-if [[ $# -lt 1 ]]; then
-  usage_text >&2
-fi
-
 while [[ $# -gt 0 ]]; do
   i=$1
   case $i in
@@ -57,14 +53,16 @@ echo "*******************"
 echo "Configuring Kpack"
 echo "*******************"
 
-# For GCR with a json key, DOCKER_USERNAME is `_json_key`
-DOCKER_USERNAME=${DOCKER_USERNAME:-"_json_key"}
-DOCKER_PASSWORD=${DOCKER_PASSWORD:-"$(cat $GCP_SERVICE_ACCOUNT_JSON_FILE)"}
-DOCKER_SERVER=${DOCKER_SERVER:-"gcr.io"}
+if [[ -n "${GCP_SERVICE_ACCOUNT_JSON_FILE}" ]]; then
+  # For GCR with a json key, DOCKER_USERNAME is `_json_key`
+  DOCKER_USERNAME=${DOCKER_USERNAME:-"_json_key"}
+  DOCKER_PASSWORD=${DOCKER_PASSWORD:-"$(cat $GCP_SERVICE_ACCOUNT_JSON_FILE)"}
+  DOCKER_SERVER=${DOCKER_SERVER:-"gcr.io"}
 
-kubectl create secret docker-registry image-registry-credentials \
-    --docker-username=$DOCKER_USERNAME --docker-password="$DOCKER_PASSWORD" --docker-server=$DOCKER_SERVER --namespace default || true
-# kubectl create secret docker-registry image-registry-credentials --docker-username="_json_key" --docker-password="$(cat /home/birdrock/workspace/credentials/cf-relint-greengrass-2826975617b2.json)" --docker-server=gcr.io --namespace default
+  kubectl create secret docker-registry image-registry-credentials \
+      --docker-username=$DOCKER_USERNAME --docker-password="$DOCKER_PASSWORD" --docker-server=$DOCKER_SERVER --namespace default || true
+  # kubectl create secret docker-registry image-registry-credentials --docker-username="_json_key" --docker-password="$(cat /home/birdrock/workspace/credentials/cf-relint-greengrass-2826975617b2.json)" --docker-server=gcr.io --namespace default
+fi
 
 kubectl apply -f config/kpack/service_account.yaml \
     -f config/kpack/cluster_stack.yaml \
@@ -80,5 +78,3 @@ kubectl apply -f dependencies/contour-1.18.1.yaml
 echo "******"
 echo "Done"
 echo "******"
-
-
