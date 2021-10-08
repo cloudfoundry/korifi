322e779e98a8b7c93662355690d48a97246fb276
Akira Wong <64824162+gnovv@users.noreply.github.com>
2021-10-08 09:49:14 -0700

    Fix package RBAC for service account lookup (#136)
    Co-authored-by: Matt Royal <mroyal@vmware.com>

--- repositories/package_repository.go
+++ repositories/package_repository.go
@@ -20,6 +20,9 @@ const (
 //+kubebuilder:rbac:groups=workloads.cloudfoundry.org,resources=cfpackages,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=workloads.cloudfoundry.org,resources=cfpackages/status,verbs=get
 
+//+kubebuilder:rbac:groups="",resources=serviceaccounts;secrets,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=serviceaccounts/status;secrets/status,verbs=get
+
 type PackageCreateMessage struct {
 	Type      string
 	AppGUID   string
