2cedec8b504f5a076485d2b5d1baf38383c684a8
Akira Wong <wakira@vmware.com>
2021-10-18 14:54:04 -0700

    add destinationGUID to CFRoute
    Co-authored-by: Dave Walter <walterda@vmware.com>

--- apis/networking/v1alpha1/cfroute_types.go
+++ apis/networking/v1alpha1/cfroute_types.go
@@ -26,6 +26,8 @@ import (
 
 // Destination defines a target for a CFRoute, does not carry meaning outside of a CF context
 type Destination struct {
+	// GUID is required to support CF V3 Destination endpoints
+	GUID string `json:"guid"`
 	// Port is optional, defaults to ProcessModel::DEFAULT_HTTP_PORT
 	Port int `json:"port,omitempty"`
 	// App ref is required, part of the identity of a running process to which traffic may be routed
