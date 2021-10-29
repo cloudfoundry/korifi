725a5ec90284bf6ca94d86f871cfc50be9573f71
Kieron Browne <kbrowne@vmware.com>
2021-10-29 11:32:07 +0000

    Tidy up logging

--- webhooks/workloads/subnamespaceanchor_validation.go
+++ webhooks/workloads/subnamespaceanchor_validation.go
@@ -2,9 +2,9 @@ package workloads
 
 import (
 	"context"
-	"time"
 
 	"code.cloudfoundry.org/cf-k8s-controllers/webhooks/registry"
+	"github.com/go-logr/logr"
 	admissionv1 "k8s.io/api/admission/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -15,15 +15,13 @@ import (
 	"sigs.k8s.io/hierarchical-namespaces/api/v1alpha2"
 )
 
-//+kubebuilder:webhook:path=/validate-hnc-x-k8s-io-v1alpha2-subnamespaceanchor,mutating=false,failurePolicy=fail,sideEffects=NoneOnDryRun,groups=hnc.x-k8s.io,resources=subnamespaceanchors,verbs=create;update;delete;connect,versions=v1alpha2,name=vsubns.kb.io,admissionReviewVersions={v1,v1beta1}
+//+kubebuilder:webhook:path=/validate-hnc-x-k8s-io-v1alpha2-subnamespaceanchor,mutating=false,failurePolicy=fail,sideEffects=NoneOnDryRun,groups=hnc.x-k8s.io,resources=subnamespaceanchors,verbs=create;update;delete,versions=v1alpha2,name=vsubns.kb.io,admissionReviewVersions={v1,v1beta1}
 //+kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=create;delete;patch
 
 const (
 	OrgNameLabel   = "cloudfoundry.org/org-name"
 	SpaceNameLabel = "cloudfoundry.org/space-name"
 )
-
-var subnsLogger = logf.Log.WithName("subns-validate")
 
 //counterfeiter:generate -o fake -fake-name SubnamespaceAnchorLister . SubnamespaceAnchorLister
 
@@ -35,6 +33,7 @@ type SubnamespaceAnchorValidation struct {
 	lister    SubnamespaceAnchorLister
 	decoder   *admission.Decoder
 	registrar *registry.Registrar
+	logger    logr.Logger
 }
 
 type mapToName func(*v1alpha2.SubnamespaceAnchor) string
@@ -50,6 +49,7 @@ var getSpaceName mapToName = func(anchor *v1alpha2.SubnamespaceAnchor) string {
 func NewSubnamespaceAnchorValidation(lister SubnamespaceAnchorLister) *SubnamespaceAnchorValidation {
 	return &SubnamespaceAnchorValidation{
 		lister: lister,
+		logger: logf.Log.WithName("subns-validate"),
 	}
 }
 
@@ -61,7 +61,6 @@ func (v *SubnamespaceAnchorValidation) SetupWebhookWithManager(mgr ctrl.Manager)
 }
 
 func (v *SubnamespaceAnchorValidation) Handle(ctx context.Context, req admission.Request) admission.Response {
-	time.Sleep(2 * time.Second)
 	anchor := &v1alpha2.SubnamespaceAnchor{}
 	if req.Operation == admissionv1.Delete {
 		return v.handleDelete(ctx, req)
@@ -69,7 +68,7 @@ func (v *SubnamespaceAnchorValidation) Handle(ctx context.Context, req admission
 
 	err := v.decoder.Decode(req, anchor)
 	if err != nil {
-		subnsLogger.Info("failed to decode subnamespace anchor", "error", err.Error())
+		v.logger.Info("failed to decode subnamespace anchor", "error", err.Error())
 		return admission.Denied("failed to decode subnamespace anchor")
 	}
 
@@ -107,15 +106,15 @@ func (v *SubnamespaceAnchorValidation) Handle(ctx context.Context, req admission
 		return v.handleUpdate(ctx, anchor, &oldAnchor, anchorType, dupError, getName)
 	}
 
-	subnsLogger.Info("connect?", "req", req)
 	return admission.Allowed("")
 }
 
 func (v *SubnamespaceAnchorValidation) handleCreate(ctx context.Context, anchor *v1alpha2.SubnamespaceAnchor, anchorType registry.RegistryType, dupError ValidationErrorCode, name string) admission.Response {
+	logger := v.logger.WithValues("anchorType", anchorType, "anchorNamespace", anchor.Namespace, "anchorName", name)
 	err := v.registrar.TryRegister(ctx, anchorType, anchor, name)
 	if err != nil {
 		if errors.IsAlreadyExists(err) {
-			subnsLogger.Info(dupError.GetMessage(), "name", name)
+			logger.Info(dupError.GetMessage())
 			return admission.Denied(dupError.Marshal())
 		}
 		return admission.Denied("failed trying to register name")
@@ -137,27 +136,28 @@ func (v *SubnamespaceAnchorValidation) handleUpdate(
 	if anchorName == oldAnchorName {
 		return admission.Allowed("")
 	}
-	subnsLogger := subnsLogger.WithValues("anchorType", anchorType, "anchorNamespace", oldAnchor.Namespace, "oldAnchorName", oldAnchorName, "newAnchorName", anchorName)
-	subnsLogger.Info("updating name")
+
+	logger := v.logger.WithValues("anchorType", anchorType, "anchorNamespace", oldAnchor.Namespace, "oldAnchorName", oldAnchorName, "newAnchorName", anchorName)
+	logger.Info("updating name")
 
 	if err := v.registrar.TryClaimLease(ctx, anchorType, oldAnchor, oldAnchorName); err != nil {
-		subnsLogger.Info("failed to obtain lease on old record", "err", err)
+		logger.Info("failed to obtain lease on old record", "err", err)
 		return admission.Denied("cannot lock old record")
 	}
 
 	if err := v.registrar.TryRegister(ctx, anchorType, anchor, anchorName); err != nil {
 		if errors.IsAlreadyExists(err) {
 			if err := v.registrar.ReleaseLease(ctx, anchorType, oldAnchor, oldAnchorName); err != nil {
-				subnsLogger.Info("failed to release lease", "error", err)
+				logger.Info("failed to release lease", "error", err)
 			}
-			subnsLogger.Info(dupError.GetMessage())
+			logger.Info(dupError.GetMessage())
 			return admission.Denied(dupError.Marshal())
 		}
 		return admission.Denied("failed trying to register name")
 	}
 
 	if err := v.registrar.DeleteRecordFor(ctx, anchorType, oldAnchor.Namespace, oldAnchorName); err != nil {
-		subnsLogger.Error(err, "failed to delete registration record", "anchorType", anchorType, "anchorNamespace", oldAnchor.Namespace, "anchorName", oldAnchorName)
+		logger.Error(err, "failed to delete registration record", "anchorType", anchorType, "anchorNamespace", oldAnchor.Namespace, "anchorName", oldAnchorName)
 	}
 
 	return admission.Allowed("")
@@ -178,7 +178,7 @@ func (v *SubnamespaceAnchorValidation) handleDelete(ctx context.Context, req adm
 
 	err := v.registrar.DeleteRecordFor(ctx, anchorType, oldAnchor.Namespace, name)
 	if err != nil {
-		subnsLogger.Error(err, "failed to delete register record", "namespace", oldAnchor.Namespace, "name", name)
+		v.logger.Error(err, "failed to delete register record", "namespace", oldAnchor.Namespace, "name", name)
 	}
 	return admission.Allowed("")
 }
