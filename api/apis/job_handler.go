3ff333061114c86f3eaacf79e4fe1a6141e316c0
Ashwin Krishna <krishnaas@vmware.com>
2021-11-08 17:01:57 -0800

    API Client can retrieve Jobs for space manifest application
    Co-authored-by: Andrew Wittrock <awittrock@vmware.com>

---
+++ api/apis/job_handler.go
@@ -0,0 +1,66 @@
+package apis
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"code.cloudfoundry.org/cf-k8s-controllers/api/presenter"
+
+	"github.com/go-logr/logr"
+	"github.com/gorilla/mux"
+)
+
+const (
+	JobGetEndpoint  = "/v3/jobs/{guid}"
+	syncSpacePrefix = "sync-space.apply_manifest-"
+)
+
+type JobHandler struct {
+	logger    logr.Logger
+	serverURL url.URL
+}
+
+func NewJobHandler(logger logr.Logger, serverURL url.URL) *JobHandler {
+	return &JobHandler{
+		logger:    logger,
+		serverURL: serverURL,
+	}
+}
+
+func (h *JobHandler) jobGetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(r)
+	jobGUID := vars["guid"]
+
+	spaceGUID := getSpaceGUID(jobGUID)
+	if spaceGUID == "" {
+		h.logger.Info("Invalid Job GUID")
+		writeNotFoundErrorResponse(w, "Job")
+		return
+	}
+
+	responseBody, err := json.Marshal(presenter.ForJob(jobGUID, spaceGUID, h.serverURL))
+	if err != nil {
+		h.logger.Error(err, "Failed to render response", "Job GUID", jobGUID)
+		writeUnknownErrorResponse(w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseBody)
+}
+
+func getSpaceGUID(jobGUID string) string {
+	if strings.HasPrefix(jobGUID, syncSpacePrefix) {
+		spaceGUID := strings.Replace(jobGUID, syncSpacePrefix, "", 1)
+		return spaceGUID
+	}
+	return ""
+}
+
+func (h *JobHandler) RegisterRoutes(router *mux.Router) {
+	router.Path(JobGetEndpoint).Methods("GET").HandlerFunc(h.jobGetHandler)
+}
---
+++ api/apis/job_handler_test.go
@@ -0,0 +1,77 @@
+package apis_test
+
+import (
+	"fmt"
+	"net/http"
+
+	"code.cloudfoundry.org/cf-k8s-controllers/api/apis"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+var _ = Describe("JobHandler", func() {
+	Describe("GET /v3/jobs endpoint", func() {
+		const (
+			spaceGUID = "my-space-guid"
+			jobGUID   = "sync-space.apply_manifest-" + spaceGUID
+		)
+
+		BeforeEach(func() {
+			jobsHandler := apis.NewJobHandler(
+				logf.Log.WithName("TestRootHandler"),
+				*serverURL,
+			)
+			jobsHandler.RegisterRoutes(router)
+		})
+
+		When("on the happy path", func() {
+			BeforeEach(func() {
+				req, err := http.NewRequest("GET", "/v3/jobs/"+jobGUID, nil)
+				Expect(err).NotTo(HaveOccurred())
+				router.ServeHTTP(rr, req)
+			})
+			It("returns status 200 OK", func() {
+				Expect(rr.Code).To(Equal(http.StatusOK), "Matching HTTP response code:")
+			})
+
+			It("returns Content-Type as JSON in header", func() {
+				contentTypeHeader := rr.Header().Get("Content-Type")
+				Expect(contentTypeHeader).To(Equal(jsonHeader), "Matching Content-Type header:")
+			})
+
+			It("matches the expected response body format", func() {
+				Expect(rr.Body.String()).To(MatchJSON(fmt.Sprintf(`{
+				  "created_at": "",
+				  "errors": null,
+				  "guid": "%[2]s",
+				  "links": {
+					"self": {
+					  "href": "%[1]s/v3/jobs/%[2]s"
+					},
+					"space": {
+					  "href": "%[1]s/v3/spaces/%[3]s"
+					}
+				  },
+				  "operation": "space.apply_manifest",
+				  "state": "COMPLETE",
+				  "updated_at": "",
+				  "warnings": null
+				}`, defaultServerURL, jobGUID, spaceGUID)), "Response body matches response:")
+			})
+		})
+
+		When("guid provided is not a valid job guid", func() {
+			BeforeEach(func() {
+				req, err := http.NewRequest("GET", "/v3/jobs/some-guid", nil)
+				Expect(err).NotTo(HaveOccurred())
+				router.ServeHTTP(rr, req)
+			})
+			It("return an error", func() {
+				expectNotFoundError("Job not found")
+			})
+		})
+
+	})
+})
