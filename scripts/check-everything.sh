e45448c84abb8bc12cb9125308ab3b31373d8548
Danail Branekov <danailster@gmail.com>
2021-11-18 18:58:38 +0200

    Run controllers/api and e2e in parallel
    Unfortunately env tests cannot be easily run in parallel, probably they
    share common environment so further parallelisation does not seem to be
    trivial.

---
+++ scripts/check-everything.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+CF_K8S_CONTROLLERS_DIR="$SCRIPT_DIR/.."
+
+RED=1
+GREEN=2
+print_message() {
+  message=$1
+  colour=$2
+  printf "\\r\\033[00;3%sm[%s]\\033[0m\\n" "$colour" "$message"
+}
+
+main() {
+  print_message "about to run tests in parallel, it will be awesome" $GREEN
+  print_message "ctrl-d panes when they are done" $RED
+  tmux new-window -n cf-k8s-tests "/bin/bash -c \"make test-controllers-api; bash --init-file <(echo 'history -s make test-controllers-api')\""
+  tmux split-window -h -p 50 "/bin/bash -c \"make test-e2e; bash --init-file <(echo 'history -s make test-e2e')\""
+}
+
+main
