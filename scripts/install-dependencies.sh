d42fc877ea0b825d08c23577670e3ffe97a38e82
Kieron Browne <kbrowne@vmware.com>
2022-01-13 10:49:03 +0000

    Commit `make fmt` changes
    The formatters were making a couple of modifications, so just commit
    them here. Whitespace only.

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -89,7 +89,7 @@ kubectl apply -f "${DEP_DIR}/kpack/cluster_stack.yaml" \
   -f "${DEP_DIR}/kpack/cluster_store.yaml"
 
 if [[ -n "${DOCKER_SERVER:=}" ]]; then
-  cat dependencies/kpack/cluster_builder.yaml | sed "s/tag: gcr.io/tag: ${DOCKER_SERVER}/g" > /tmp/clusterbuilder.yml
+  cat dependencies/kpack/cluster_builder.yaml | sed "s/tag: gcr.io/tag: ${DOCKER_SERVER}/g" >/tmp/clusterbuilder.yml
   kubectl apply -f /tmp/clusterbuilder.yml
 else
   kubectl apply -f "${DEP_DIR}/kpack/cluster_builder.yaml"
