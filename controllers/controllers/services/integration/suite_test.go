3647b625b58b5d0f66e0e6a64b5974debe57cd29
Matt Royal <mroyal@vmware.com>
2022-01-20 16:48:21 -0800

    Add CFServiceInstance and CFServiceBinding resources
    Note: the `kubebuilder create api` command failed due to our unusual
    project structure. Here's what we had to do:
    
    - cd into `controllers`
    - run the `kubebuilder create api` command, which will generate files,
      but fail later
    - manually inspect the generated files and update the import paths to
      convert `code.cloudfoundry.org/cf-k8s-controllers/apis`
      to `code.cloudfoundry.org/cf-k8s-controllers/controllers/apis`
    - manually update the PROJECT file to point to the correct path
    
    [Issue #431]
    
    Co-authored-by: Matt Royal <mroyal@vmware.com>
    Co-authored-by: Ashwin Krishna <krishnaas@vmware.com>

--- controllers/controllers/services/integration/suite_test.go
+++ controllers/controllers/services/integration/suite_test.go
@@ -23,6 +23,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -35,6 +36,8 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
+var cfg *rest.Config
+var _ = cfg // make the linter happy
 var k8sClient client.Client
 var testEnv *envtest.Environment
 
