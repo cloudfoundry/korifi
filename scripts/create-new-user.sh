866184111b2eda5de993af99eb8157ea0c3034b8
Dave Walter <walterda@vmware.com>
2022-01-27 14:01:00 -0800

    Add helpful instruction to create-new-user script
    ... to tell users how to grant space developer permissions to the new
    user

--- scripts/create-new-user.sh
+++ scripts/create-new-user.sh
@@ -15,21 +15,27 @@ username="$1"
 priv_key_file="$(mktemp)"
 csr_file="$(mktemp)"
 cert_file="$(mktemp)"
-csr_name="$(echo $RANDOM | shasum | head -c 40)"
+csr_name="$(echo ${RANDOM} | shasum | head -c 40)"
 
-openssl req -new -newkey rsa:4096 -keyout "$priv_key_file" -nodes -out "$csr_file" -subj "/CN=$username"
+openssl req -new -newkey rsa:4096 -keyout "${priv_key_file}" -nodes -out "${csr_file}" -subj "/CN=${username}"
 
 cat <<EOF | kubectl create -f -
 apiVersion: certificates.k8s.io/v1
 kind: CertificateSigningRequest
 metadata:
-  name: $csr_name
+  name: ${csr_name}
 spec:
   signerName: "kubernetes.io/kube-apiserver-client"
-  request: "$(base64 "$csr_file" | tr -d '\n')"
+  request: "$(base64 "${csr_file}" | tr -d '\n')"
   usages:
   - client auth
 EOF
-kubectl certificate approve "$csr_name"
-kubectl get csr "$csr_name" -o jsonpath='{.status.certificate}' | base64 --decode >"$cert_file"
-kubectl config set-credentials "$username" --client-certificate="$cert_file" --client-key="$priv_key_file" --embed-certs
+
+kubectl certificate approve "${csr_name}"
+kubectl get csr "${csr_name}" -o jsonpath='{.status.certificate}' | base64 --decode >"${cert_file}"
+kubectl config set-credentials "${username}" --client-certificate="${cert_file}" --client-key="${priv_key_file}" --embed-certs
+
+cat<<EOF
+
+Use "cf set-space-role ${username} ORG SPACE SpaceDeveloper" to grant this user permissions in a space.
+EOF
