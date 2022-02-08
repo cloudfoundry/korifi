ddd2b2d3d1f6b3296a02cd768c493311b37cffd1
Akira Wong <wakira@vmware.com>
2022-02-08 11:22:00 -0800

    598 changed process controller to use ServcBind indexer
    Co-authored-by: Akira Wong <wakira@vmware.com>

--- controllers/controllers/shared/index.go
+++ controllers/controllers/shared/index.go
@@ -3,15 +3,30 @@ package shared
 import (
 	"context"
 
+	servicesv1alpha1 "code.cloudfoundry.org/cf-k8s-controllers/controllers/apis/services/v1alpha1"
+
 	networkingv1alpha1 "code.cloudfoundry.org/cf-k8s-controllers/controllers/apis/networking/v1alpha1"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
-const DestinationAppName = "destinationAppName"
+const (
+	DestinationAppName         = "destinationAppName"
+	ServiceBindingAppGUIDIndex = "serviceBindingAppGUID"
+)
 
 func SetupIndexWithManager(mgr manager.Manager) error {
+	if err := setupRouteIndex(mgr); err != nil {
+		return err
+	}
+	if err := setupServiceBindingIndex(mgr); err != nil {
+		return err
+	}
+	return nil
+}
+
+func setupRouteIndex(mgr manager.Manager) error {
 	// Generate indexes for CFRoute on field spec.Destination.AppRef.Name for efficient querying.
 	if err := mgr.GetFieldIndexer().IndexField(context.Background(), &networkingv1alpha1.CFRoute{}, DestinationAppName,
 		func(rawObj client.Object) []string {
@@ -22,6 +37,18 @@ func SetupIndexWithManager(mgr manager.Manager) error {
 			}
 			return destinationAppNames
 		}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setupServiceBindingIndex(mgr manager.Manager) error {
+	// Generate indexes for CFServiceBinding on field serviceBinding.Spec.AppRef.Name for efficient querying.
+	if err := mgr.GetFieldIndexer().IndexField(context.Background(), &servicesv1alpha1.CFServiceBinding{}, ServiceBindingAppGUIDIndex, func(rawObj client.Object) []string {
+		serviceBinding := rawObj.(*servicesv1alpha1.CFServiceBinding)
+		return []string{serviceBinding.Spec.AppRef.Name}
+	}); err != nil {
 		return err
 	}
 
