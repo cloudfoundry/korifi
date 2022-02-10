377fb66703765a079fd1a172b763a360a9367e5c
Akira Wong <wakira@vmware.com>
2022-02-09 16:36:30 -0800

    Updated ServiceInstance Secret to include type
    Co-authored-by: Julian Hjortshoj <hjortshojj@vmware.com>

--- controllers/apis/services/v1alpha1/cfserviceinstance_types.go
+++ controllers/apis/services/v1alpha1/cfserviceinstance_types.go
@@ -24,6 +24,10 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+const (
+	UserProvidedType = "user-provided"
+)
+
 // CFServiceInstanceSpec defines the desired state of CFServiceInstance
 type CFServiceInstanceSpec struct {
 	// Name defines the name of the Service Instance
