a61d9bdaecf5be86fe9f9476876c26add9b22daa
Julian Hjortshoj <hjortshojj@vmware.com>
2022-02-25 14:23:26 -0800

    WIP
    Use Ginkgo Ordered tests to avoid repeated test setups
    
    - This is a WIP commit to test out the approach
    - If we were to adopt the approach, we would likely want to move the
      spaceDev and spaceMgr client setup into the BeforeSuite and move the
      role setup into the createOrg and createSpace setup calls so that we
      don't have to do it in tests.
    - Applying these changes reduced the overall test time for the 6
      affected tests from 3m34s to 1m12s.

--- api/tests/e2e/service_bindings_test.go
+++ api/tests/e2e/service_bindings_test.go
@@ -1,6 +1,7 @@
 package e2e_test
 
 import (
+	certsv1 "k8s.io/api/certificates/v1"
 	"net/http"
 	"time"
 
@@ -11,8 +12,8 @@ import (
 	rbacv1 "k8s.io/api/rbac/v1"
 )
 
-var _ = Describe("Service Bindings", func() {
-	Describe("List", func() {
+var _ = FDescribe("Service Bindings", func() {
+	Describe("List", Ordered, func() {
 		var (
 			appGUID      string
 			orgGUID      string
@@ -22,28 +23,61 @@ var _ = Describe("Service Bindings", func() {
 			httpError    error
 			queryString  string
 			result       resourceListWithInclusion
+
+			spaceDevUserName   string
+			spaceDevSigningReq *certsv1.CertificateSigningRequest
+			// spaceDevAuthHeader string
+			spaceDevPEM    string
+			spaceDevClient *resty.Client
+
+			spaceMgrUserName   string
+			spaceMgrSigningReq *certsv1.CertificateSigningRequest
+			// spaceMgrAuthHeader string
+			spaceMgrPEM    string
+			spaceMgrClient *resty.Client
+			testClient     *resty.Client
 		)
 
-		BeforeEach(func() {
+		BeforeAll(func() {
+			spaceDevUserName = generateGUID("space-dev-user")
+			spaceDevSigningReq, spaceDevPEM = obtainClientCert(spaceDevUserName)
+			// spaceDevAuthHeader = "ClientCert " + spaceDevPEM
+			spaceDevClient = resty.New().SetBaseURL(apiServerRoot).SetAuthScheme("ClientCert").SetAuthToken(spaceDevPEM)
+
+			spaceMgrUserName = generateGUID("space-mgr-user")
+			spaceMgrSigningReq, spaceMgrPEM = obtainClientCert(spaceMgrUserName)
+			// spaceMgrAuthHeader = "ClientCert " + spaceMgrPEM
+			spaceMgrClient = resty.New().SetBaseURL(apiServerRoot).SetAuthScheme("ClientCert").SetAuthToken(spaceMgrPEM)
+
 			orgGUID = createOrg(generateGUID("org"))
 			time.Sleep(time.Second) // this appears to reduce flakes, but should be removed once we have better logic to determine org/space readiness
 			spaceGUID = createSpace(generateGUID("space1"), orgGUID)
 			time.Sleep(time.Second)
-			createOrgRole("organization_user", rbacv1.UserKind, certUserName, orgGUID)
 			instanceGUID = createServiceInstance(spaceGUID, generateGUID("service-instance"))
 			appGUID = createApp(spaceGUID, generateGUID("app"))
 			createServiceBinding(appGUID, instanceGUID)
 
+			createOrgRole("organization_user", rbacv1.UserKind, certUserName, orgGUID)
+			createOrgRole("organization_user", rbacv1.UserKind, spaceDevUserName, orgGUID)
+			createOrgRole("organization_user", rbacv1.UserKind, spaceMgrUserName, orgGUID)
+			createSpaceRole("space_manager", rbacv1.UserKind, spaceMgrUserName, spaceGUID)
+			createSpaceRole("space_developer", rbacv1.UserKind, spaceDevUserName, spaceGUID)
+		})
+
+		AfterAll(func() {
+			deleteOrg(orgGUID)
+			deleteCSR(spaceDevSigningReq)
+			deleteCSR(spaceMgrSigningReq)
+		})
+
+		BeforeEach(func() {
+			testClient = certClient
 			queryString = ""
 			result = resourceListWithInclusion{}
 		})
 
 		JustBeforeEach(func() {
-			httpResp, httpError = certClient.R().SetResult(&result).Get("/v3/service_credential_bindings" + queryString)
-		})
-
-		AfterEach(func() {
-			deleteOrg(orgGUID)
+			httpResp, httpError = testClient.R().SetResult(&result).Get("/v3/service_credential_bindings" + queryString)
 		})
 
 		It("Returns an empty list", func() {
@@ -54,7 +88,7 @@ var _ = Describe("Service Bindings", func() {
 
 		When("the user has space manager role", func() {
 			BeforeEach(func() {
-				createSpaceRole("space_manager", rbacv1.UserKind, certUserName, spaceGUID)
+				testClient = spaceMgrClient
 			})
 
 			It("succeeds", func() {
@@ -66,7 +100,7 @@ var _ = Describe("Service Bindings", func() {
 
 		When("the user has space developer role", func() {
 			BeforeEach(func() {
-				createSpaceRole("space_developer", rbacv1.UserKind, certUserName, spaceGUID)
+				testClient = spaceDevClient
 			})
 
 			It("succeeds", func() {
