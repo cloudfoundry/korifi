395a1b08b4ad87e1b581f09027debb1897ea83f1
Akira Wong <wakira@vmware.com>
2022-03-03 13:44:44 -0800

    #704 Remove sleep from space handler
    Co-authored-by: Dave Walter <walterda@vmware.com>

--- api/apis/space_handler.go
+++ api/apis/space_handler.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
-	"time"
 
 	"github.com/go-logr/logr"
 	"github.com/google/uuid"
@@ -112,7 +111,6 @@ func (h *SpaceHandler) SpaceCreateHandler(info authorization.Info, w http.Respon
 
 	spaceResponse := presenter.ForCreateSpace(record, h.apiBaseURL)
 	writeResponse(w, http.StatusCreated, spaceResponse)
-	time.Sleep(2 * time.Second) // TODO this is hideous
 }
 
 func (h *SpaceHandler) SpaceListHandler(info authorization.Info, w http.ResponseWriter, r *http.Request) {
