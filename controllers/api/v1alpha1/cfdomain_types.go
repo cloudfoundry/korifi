27b547b164d046f3dd27fe4f4a35d5d12baa60e2
Akira Wong <wakira@vmware.com>
2022-06-01 14:40:54 -0700

    commit to bump CI
    Co-authored-by: Andrew Costa <ancosta@vmware.com>

--- controllers/api/v1alpha1/cfdomain_types.go
+++ controllers/api/v1alpha1/cfdomain_types.go
@@ -28,7 +28,7 @@ type CFDomainSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Domain name is required and conforms to RFC 1035
+	// Domain name is required and must conform to RFC 1035
 	// +kubebuilder:validation:Pattern="^[a-zA-Z]([a-zA-Z0-9-]*[a-zA-Z0-9])?(\\.[a-zA-Z]([a-zA-Z0-9-]*[a-zA-Z0-9])?)*$"
 	Name string `json:"name"`
 }
