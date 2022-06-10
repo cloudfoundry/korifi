bb65b5abc5b43d45ceb2c0e481551d84a119209a
Kieron Browne <kbrowne@vmware.com>
2022-06-10 11:24:02 +0000

    Refactor CFTask controller
    Extract logical bits into private functions
    
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- controllers/controllers/workloads/cftask_controller.go
+++ controllers/controllers/workloads/cftask_controller.go
@@ -77,42 +77,56 @@ func (r *CFTaskReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		return ctrl.Result{}, err
 	}
 
-	if cfTask.Status.SequenceID == 0 {
-		cfTaskCopy := cfTask.DeepCopy()
-		cfTaskCopy.Status.SequenceID, err = r.seqIdGenerator.Generate()
-		if err != nil {
-			r.logger.Info("error-generating-sequence-id", "error", err)
-			return ctrl.Result{}, err
-		}
-
-		err = r.k8sClient.Status().Patch(ctx, cfTaskCopy, client.MergeFrom(cfTask))
-		if err != nil {
-			r.logger.Info("error-updating-status", "error", err)
-			return ctrl.Result{}, err
-		}
+	err = r.ensureSequenceId(ctx, cfTask)
+	if err != nil {
+		return ctrl.Result{}, err
 	}
 
+	cfApp, err := r.getApp(ctx, cfTask)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	cfDroplet, err := r.getDroplet(ctx, cfTask, cfApp)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, r.createEiriniTask(ctx, cfTask, cfDroplet)
+}
+
+func (r *CFTaskReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&korifiv1alpha1.CFTask{}).
+		Complete(r)
+}
+
+func (r *CFTaskReconciler) getApp(ctx context.Context, cfTask *korifiv1alpha1.CFTask) (*korifiv1alpha1.CFApp, error) {
 	cfApp := new(korifiv1alpha1.CFApp)
-	err = r.k8sClient.Get(ctx, types.NamespacedName{
-		Namespace: req.Namespace,
+	err := r.k8sClient.Get(ctx, types.NamespacedName{
+		Namespace: cfTask.Namespace,
 		Name:      cfTask.Spec.AppRef.Name,
 	}, cfApp)
 	if err != nil {
 		r.logger.Info("error-getting-cfapp", "error", err)
 		if k8serrors.IsNotFound(err) {
-			r.recorder.Eventf(cfTask, "Warning", "appNotFound", "Did not find app with name %s in namespace %s", cfTask.Spec.AppRef.Name, req.Namespace)
+			r.recorder.Eventf(cfTask, "Warning", "appNotFound", "Did not find app with name %s in namespace %s", cfTask.Spec.AppRef.Name, cfTask.Namespace)
 		}
-		return ctrl.Result{}, err
+		return nil, err
 	}
 
 	if cfApp.Spec.CurrentDropletRef.Name == "" {
 		r.recorder.Eventf(cfTask, "Warning", "appCurrentDropletRefNotSet", "App %s does not have a current droplet", cfTask.Spec.AppRef.Name)
-		return ctrl.Result{}, errors.New("app droplet ref not set")
+		return nil, errors.New("app droplet ref not set")
 	}
 
+	return cfApp, nil
+}
+
+func (r *CFTaskReconciler) getDroplet(ctx context.Context, cfTask *korifiv1alpha1.CFTask, cfApp *korifiv1alpha1.CFApp) (*korifiv1alpha1.CFBuild, error) {
 	cfDroplet := new(korifiv1alpha1.CFBuild)
-	err = r.k8sClient.Get(ctx, types.NamespacedName{
-		Namespace: req.Namespace,
+	err := r.k8sClient.Get(ctx, types.NamespacedName{
+		Namespace: cfApp.Namespace,
 		Name:      cfApp.Spec.CurrentDropletRef.Name,
 	}, cfDroplet)
 	if err != nil {
@@ -120,18 +134,22 @@ func (r *CFTaskReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 			r.recorder.Eventf(cfTask, "Warning", "appCurrentDropletNotFound", "Current droplet %s for app %s does not exist", cfApp.Spec.CurrentDropletRef.Name, cfTask.Spec.AppRef.Name)
 		}
 		r.logger.Info("error-getting-cfdroplet", "error", err)
-		return ctrl.Result{}, err
+		return nil, err
 	}
 
 	if cfDroplet.Status.Droplet == nil {
 		r.recorder.Eventf(cfTask, "Warning", "dropletBuildStatusNotSet", "Current droplet %s from app %s does not have a droplet image", cfApp.Spec.CurrentDropletRef.Name, cfTask.Spec.AppRef.Name)
-		return ctrl.Result{}, errors.New("droplet build status not set")
+		return nil, errors.New("droplet build status not set")
 	}
 
+	return cfDroplet, nil
+}
+
+func (r *CFTaskReconciler) createEiriniTask(ctx context.Context, cfTask *korifiv1alpha1.CFTask, cfDroplet *korifiv1alpha1.CFBuild) error {
 	eiriniTask := &eiriniv1.Task{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      cfTask.Name,
-			Namespace: req.Namespace,
+			Namespace: cfTask.Namespace,
 			Labels: map[string]string{
 				korifiv1alpha1.CFTaskGUIDLabelKey: cfTask.Name,
 			},
@@ -143,22 +161,35 @@ func (r *CFTaskReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		},
 	}
 
-	err = r.k8sClient.Create(ctx, eiriniTask)
+	err := r.k8sClient.Create(ctx, eiriniTask)
 	if err != nil {
 		r.logger.Info("error-creating-eirini-task", "error", err)
 		if k8serrors.IsAlreadyExists(err) {
-			return ctrl.Result{}, nil
+			return nil
 		}
-		return ctrl.Result{}, err
+		return err
 	}
 	r.recorder.Eventf(cfTask, "Normal", "taskCreated", "Created eirini task %s", eiriniTask.Name)
 
-	return ctrl.Result{}, nil
+	return nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
-func (r *CFTaskReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&korifiv1alpha1.CFTask{}).
-		Complete(r)
+func (r *CFTaskReconciler) ensureSequenceId(ctx context.Context, cfTask *korifiv1alpha1.CFTask) error {
+	if cfTask.Status.SequenceID == 0 {
+		cfTaskCopy := cfTask.DeepCopy()
+		var err error
+		cfTaskCopy.Status.SequenceID, err = r.seqIdGenerator.Generate()
+		if err != nil {
+			r.logger.Info("error-generating-sequence-id", "error", err)
+			return err
+		}
+
+		err = r.k8sClient.Status().Patch(ctx, cfTaskCopy, client.MergeFrom(cfTask))
+		if err != nil {
+			r.logger.Info("error-updating-status", "error", err)
+			return err
+		}
+	}
+
+	return nil
 }
