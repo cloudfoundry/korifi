b01c0fd1765d1703b56440010eed797c43328079
Matt Royal <pivotal-relint@pivotal.io>
2022-06-14 10:41:27 -0700

    WIP

--- statefulset-runner/scripts/run_unit_tests.sh
+++ statefulset-runner/scripts/run_unit_tests.sh
@@ -12,7 +12,7 @@ main() {
 
 run_tests() {
   pushd "$BASEDIR" >/dev/null || exit 1
-  go run github.com/onsi/ginkgo/v2/ginkgo --mod=vendor -p -r --keep-going --skip-package=tests --randomize-all --randomize-suites $@
+  go run github.com/onsi/ginkgo/v2/ginkgo -p -r --keep-going --skip-package=tests --randomize-all --randomize-suites $@
   popd >/dev/null || exit 1
 }
 
