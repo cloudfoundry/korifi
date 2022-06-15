ed779634dfb5fc335bf39391d292b1d97e4deea9
Giuseppe Capizzi <gcapizzi@vmware.com>
2022-06-15 15:25:47 +0000

    Refactor the smoke test suite
    Main change is to rework the `cf login` so it will select the correct
    number for the given user.
    
    Also bump cf-test-helpers to a recent version where is go.mod enabled
    and containing the 'with-stdin' runner.
    
    Issue: https://github.com/cloudfoundry/korifi/issues/1014
    
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- tests/smoke/smoke_test.go
+++ tests/smoke/smoke_test.go
@@ -3,27 +3,27 @@ package smoke_test
 import (
 	"crypto/tls"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
-	. "github.com/onsi/ginkgo"
+	"github.com/cloudfoundry/cf-test-helpers/cf"
+	"github.com/cloudfoundry/cf-test-helpers/generator"
+	. "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/ginkgo/v2/types"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
-
-	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
-	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
 )
 
 const NamePrefix = "cf-on-k8s-smoke"
 
 func GetRequiredEnvVar(envVarName string) string {
 	value, ok := os.LookupEnv(envVarName)
-	if !ok {
-		panic(envVarName + " environment variable is required, but was not provided.")
-	}
+	Expect(ok).To(BeTrue(), envVarName+" environment variable is required, but was not provided.")
 	return value
 }
 
@@ -44,32 +44,19 @@ var _ = Describe("Smoke Tests", func() {
 		)
 
 		BeforeEach(func() {
-			doLogin := GetDefaultedEnvVar("SMOKE_TEST_LOGIN", "")
-
-			if doLogin != "" {
-				apiEndpoint := GetRequiredEnvVar("SMOKE_TEST_API_ENDPOINT")
-				// username := GetRequiredEnvVar("SMOKE_TEST_USERNAME")
-				// password := GetRequiredEnvVar("SMOKE_TEST_PASSWORD")
-
-				apiArguments := []string{"api", apiEndpoint}
-				skip_ssl, _ := os.LookupEnv("SMOKE_TEST_SKIP_SSL")
-				skip_ssl_bool := skip_ssl == "true"
-				if skip_ssl_bool {
-					apiArguments = append(apiArguments, "--skip-ssl-validation")
-				}
-				http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: skip_ssl_bool}
-
-				// Target CF and auth
-				cfAPI := cf.Cf(apiArguments...)
-				Eventually(cfAPI).Should(Exit(0))
-
-				// Authenticate - untested not sure if this works for cf-on-k8s
-				Eventually(cf.Cf("login").Wait()).Should(Exit(0))
+			apiArguments := []string{"api", GetRequiredEnvVar("SMOKE_TEST_API_ENDPOINT")}
+			skipSSL := os.Getenv("SMOKE_TEST_SKIP_SSL") == "true"
+			if skipSSL {
+				apiArguments = append(apiArguments, "--skip-ssl-validation")
 			}
+
+			cfAPI := cf.Cf(apiArguments...)
+			Eventually(cfAPI).Should(Exit(0))
+
+			loginAs(GetRequiredEnvVar("SMOKE_TEST_USER"))
 
 			appRouteProtocol = GetDefaultedEnvVar("SMOKE_TEST_APP_ROUTE_PROTOCOL", "https")
 			appsDomain = GetRequiredEnvVar("SMOKE_TEST_APPS_DOMAIN")
-			// Create an org and space and target
 			orgName = generator.PrefixedRandomName(NamePrefix, "org")
 			spaceName := generator.PrefixedRandomName(NamePrefix, "space")
 
@@ -79,12 +66,11 @@ var _ = Describe("Smoke Tests", func() {
 		})
 
 		AfterEach(func() {
-			if CurrentGinkgoTestDescription().Failed {
+			if CurrentSpecReport().State.Is(types.SpecStateFailed) {
 				printAppReport(appName)
 			}
 
 			if orgName != "" {
-				// Delete the test org
 				Eventually(func() *Session {
 					return cf.Cf("delete-org", orgName, "-f").Wait()
 				}, 2*time.Minute, 1*time.Second).Should(Exit(0))
@@ -94,31 +80,20 @@ var _ = Describe("Smoke Tests", func() {
 		It("creates a routable app pod in Kubernetes from a source-based app", func() {
 			appName = generator.PrefixedRandomName(NamePrefix, "app")
 
-			By("pushing an app and checking that the CF CLI command succeeds")
 			cfPush := cf.Cf("push", appName, "-p", "assets/test-node-app")
 			Eventually(cfPush).Should(Exit(0))
 
-			By("querying the app")
-			var resp *http.Response
+			var httpClient http.Client
+			httpClient.Transport = &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			}
 
-			Eventually(func() int {
-				var err error
-				http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-				resp, err = http.Get(fmt.Sprintf("%s://%s.%s", appRouteProtocol, appName, appsDomain))
-				Expect(err).NotTo(HaveOccurred())
-				return resp.StatusCode
-			}, 2*time.Minute, 30*time.Second).Should(Equal(200))
-
-			body, err := ioutil.ReadAll(resp.Body)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(string(body)).To(Equal("Hello World\n"))
-
-			// TODO: Log retrieval is not supported yet
-			//By("verifying that the application's logs are available.")
-			//Eventually(func() string {
-			//	cfLogs := cf.Cf("logs", appName, "--recent")
-			//	return string(cfLogs.Wait().Out.Contents())
-			//}, 2*time.Minute, 2*time.Second).Should(ContainSubstring("Console output from test-node-app"))
+			Eventually(func(g Gomega) {
+				resp, err := httpClient.Get(fmt.Sprintf("%s://%s.%s", appRouteProtocol, appName, appsDomain))
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(resp).To(HaveHTTPStatus(http.StatusOK))
+				g.Expect(resp).To(HaveHTTPBody(ContainSubstring("Hello World")))
+			}, 2*time.Minute, 30*time.Second).Should(Succeed())
 		})
 	})
 })
@@ -137,4 +112,20 @@ func printAppReport(appName string) {
 func printAppReportBanner(announcement string) {
 	sequence := strings.Repeat("*", len(announcement))
 	fmt.Fprintf(GinkgoWriter, "\n\n%s\n%s\n%s\n", sequence, announcement, sequence)
+}
+
+func loginAs(user string) {
+	r, w := io.Pipe()
+	loginSession := cf.CfWithStdin(r, "login")
+	Eventually(loginSession).Should(Say("Choose your Kubernetes"))
+
+	userEntryRegex, err := regexp.Compile(`(\d*). ` + user + `\s`)
+	Expect(err).NotTo(HaveOccurred())
+	matches := userEntryRegex.FindSubmatch(loginSession.Buffer().Contents())
+	Expect(matches).To(HaveLen(2))
+
+	Expect(fmt.Fprintf(w, "%s\n\n", matches[1])).To(BeNumerically(">=", 3))
+	Expect(w.Close()).To(Succeed())
+
+	Eventually(loginSession).Should(Exit(0))
 }
