723a91c1d94bf84079eff329972f3c5578887e27
Danail Branekov <danailster@gmail.com>
2022-07-12 14:52:55 +0000

    Introduce HTTP request/response logging
    - Output URL, method, contentLength, remoteAddr for requests
    - Output statusCode, size, duration for responses
    - Correlation IDs included with both
    
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- api/main.go
+++ api/main.go
@@ -28,7 +28,6 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
-
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
@@ -289,6 +288,7 @@ func main() {
 	authInfoParser := authorization.NewInfoParser()
 	router.Use(
 		handlers.NewCorrelationIDMiddleware().Middleware,
+		handlers.NewHTTPLogging().Middleware,
 		handlers.NewAuthenticationMiddleware(
 			authInfoParser,
 			cachingIdentityProvider,
