22818d5ecce15c1393e14229e901f3107242c0c6
Danail Branekov <danailster@gmail.com>
2022-08-01 10:52:18 +0000

    Bump eirini-controller to v0.12
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -115,7 +115,7 @@ echo "*******************"
 echo "Installing Eirini"
 echo "*******************"
 
-EIRINI_VERSION="${EIRINI_VERSION:-0.11.0}"
+EIRINI_VERSION="${EIRINI_VERSION:-0.12.0}"
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
