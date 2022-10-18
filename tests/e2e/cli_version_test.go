b40c55487c0469af901f7a11c85a551af148c944
Danail Branekov <danailster@gmail.com>
2022-10-18 17:01:48 +0300

    Validate CF cli version
    Introduce an API middleware that returns an error if the user agen is CF
    cli and its version is `< 8.5.0`
    
    Issue: https://github.com/cloudfoundry/korifi/issues/1771
    
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

---
+++ tests/e2e/cli_version_test.go
@@ -0,0 +1,32 @@
+package e2e_test
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/go-http-utils/headers"
+	"github.com/go-resty/resty/v2"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Min CF CLI Version", func() {
+	var (
+		client   *resty.Client
+		httpResp *resty.Response
+	)
+
+	BeforeEach(func() {
+		client = resty.New().SetBaseURL(apiServerRoot).SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	})
+
+	JustBeforeEach(func() {
+		var err error
+		httpResp, err = client.SetHeader(headers.UserAgent, "cf/0.0.1").R().Get("/v3")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("validates the minimum cli version", func() {
+		Expect(httpResp).To(HaveRestyStatusCode(http.StatusBadRequest))
+	})
+})
