3ff5c0686ae67944cf1f6aba3534d67d55409b98
Giuseppe Capizzi <gcapizzi@vmware.com>
2023-01-18 16:26:35 +0000

    Implement list users as a stub
    When usernames are passed as a filter, this returns a list of resources
    with username and guid containing those values.
    
    This (temporary) solution makes `cf unset-org|space-role` work.
    
    Issue: https://github.com/cloudfoundry/korifi/issues/1239
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

---
+++ api/handlers/user_test.go
@@ -0,0 +1,82 @@
+package handlers_test
+
+import (
+	"net/http"
+
+	"code.cloudfoundry.org/korifi/api/handlers"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("User", func() {
+	var query string
+
+	BeforeEach(func() {
+		query = ""
+		userHandler := handlers.NewUser(*serverURL)
+		routerBuilder.LoadRoutes(userHandler)
+	})
+
+	JustBeforeEach(func() {
+		req, err := http.NewRequestWithContext(ctx, "GET", "/v3/users"+query, nil)
+		Expect(err).NotTo(HaveOccurred())
+		routerBuilder.Build().ServeHTTP(rr, req)
+	})
+
+	Describe("GET /v3/users", func() {
+		When("no parameters are provided", func() {
+			It("returns an empty list", func() {
+				Expect(rr).To(HaveHTTPStatus(http.StatusOK))
+				Expect(rr).To(HaveHTTPHeaderWithValue("Content-Type", "application/json"))
+				Expect(rr).To(HaveHTTPBody(MatchJSON(`{
+					"pagination": {
+						"total_results": 0,
+						"total_pages": 1,
+						"first": {
+							"href": "https://api.example.org/v3/users"
+						},
+						"last": {
+							"href": "https://api.example.org/v3/users"
+						},
+						"next": null,
+						"previous": null
+					},
+					"resources": []
+				}`)))
+			})
+		})
+
+		When("usernames are passed", func() {
+			BeforeEach(func() {
+				query = "?usernames=foo,bar"
+			})
+
+			It("returns a list of users matching the usernames", func() {
+				Expect(rr).To(HaveHTTPStatus(http.StatusOK))
+				Expect(rr).To(HaveHTTPHeaderWithValue("Content-Type", "application/json"))
+				Expect(rr).To(HaveHTTPBody(MatchJSON(`{
+					"pagination": {
+						"total_results": 2,
+						"total_pages": 1,
+						"first": {
+							"href": "https://api.example.org/v3/users?usernames=foo,bar"
+						},
+						"last": {
+							"href": "https://api.example.org/v3/users?usernames=foo,bar"
+						},
+						"next": null,
+						"previous": null
+					},
+					"resources": [
+						{
+							"username": "foo"
+						},
+						{
+							"username": "bar"
+						}
+					]
+				}`)))
+			})
+		})
+	})
+})
