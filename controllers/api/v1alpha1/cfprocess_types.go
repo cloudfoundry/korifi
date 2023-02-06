7cb7a2f8d536799add8fa3f697b5902c3545dbdf
Danail Branekov <danailster@gmail.com>
2023-02-06 11:14:40 +0000

    Mark unused false positives as nolint
    The latest unused linter in golangci-lint v1.51.1 is flagging these
    fields as unused incorrectly.
    
    Ideally remove nolint annotations when this is fixed in a later
    golangci-lint version
    
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- controllers/api/v1alpha1/cfprocess_types.go
+++ controllers/api/v1alpha1/cfprocess_types.go
@@ -75,10 +75,10 @@ type HealthCheckType string
 // HealthCheckData used to pass through input parameters to liveness probe
 type HealthCheckData struct {
 	// The http endpoint to use with "http" healthchecks
-	HTTPEndpoint string `json:"httpEndpoint,omitempty"`
+	HTTPEndpoint string `json:"httpEndpoint,omitempty"` //nolint:unused
 
-	InvocationTimeoutSeconds int64 `json:"invocationTimeoutSeconds"`
-	TimeoutSeconds           int64 `json:"timeoutSeconds"`
+	InvocationTimeoutSeconds int64 `json:"invocationTimeoutSeconds"` //nolint:unused
+	TimeoutSeconds           int64 `json:"timeoutSeconds"`           //nolint:unused
 }
 
 // CFProcessStatus defines the observed state of CFProcess
