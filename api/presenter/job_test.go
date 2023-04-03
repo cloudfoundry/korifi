ce26fb40f96a95e1516d59358694627196785158
Giuseppe Capizzi <gcapizzi@vmware.com>
2023-04-03 13:07:06 +0000

    Move json from handlers/job_test to presenters
    Issue: #2255
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>
    Co-authored-by: Giuseppe Capizzi <gcapizzi@vmware.com>

---
+++ api/presenter/job_test.go
@@ -0,0 +1,81 @@
+package presenter_test
+
+import (
+	"encoding/json"
+	"net/url"
+
+	"code.cloudfoundry.org/korifi/api/presenter"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("", func() {
+	var (
+		baseURL *url.URL
+		output  []byte
+	)
+
+	BeforeEach(func() {
+		var err error
+		baseURL, err = url.Parse("https://api.example.org")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	Describe("ForManifestApplyJob", func() {
+		JustBeforeEach(func() {
+			response := presenter.ForManifestApplyJob("the-job-guid", "the-space-guid", *baseURL)
+			var err error
+			output, err = json.Marshal(response)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("renders the job", func() {
+			Expect(output).To(MatchJSON(`{
+				"created_at": "",
+				"errors": null,
+				"guid": "the-job-guid",
+				"links": {
+					"self": {
+						"href": "https://api.example.org/v3/jobs/the-job-guid"
+					},
+					"space": {
+						"href": "https://api.example.org/v3/spaces/the-space-guid"
+					}
+				},
+				"operation": "space.apply_manifest",
+				"state": "COMPLETE",
+				"updated_at": "",
+				"warnings": null
+			}`))
+		})
+	})
+
+	Describe("ForDeleteJob", func() {
+		JustBeforeEach(func() {
+			response := presenter.ForJob("the-job-guid", "the.operation", *baseURL)
+			var err error
+			output, err = json.Marshal(response)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("renders the job", func() {
+			Expect(output).To(MatchJSON(`{
+				"created_at": "",
+				"errors": null,
+				"guid": "the-job-guid",
+				"links": {
+					"self": {
+						"href": "https://api.example.org/v3/jobs/the-job-guid"
+					}
+				},
+				"operation": "the.operation",
+				"state": "COMPLETE",
+				"updated_at": "",
+				"warnings": null
+			}`))
+		})
+	})
+
+	Describe("JobURLForRedirects", func() {
+	})
+})
