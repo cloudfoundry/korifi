92831d6399864b57ff5747f654c4215d9f1e9588
Kieron Browne <kbrowne@vmware.com>
2023-05-31 15:29:21 +0100

    Use webhook to reset the version annotation if removed
    Given that it's the webhook's responsibility to add the creation-version
    annotation on resource creation, we also ensure it is restored when an
    update removes it.
    
    Co-authored-by: Giuseppe Capizzi <gcapizzi@vmware.com>

--- controllers/webhooks/version/version_webhook.go
+++ controllers/webhooks/version/version_webhook.go
@@ -1,6 +1,6 @@
 package version
 
-//+kubebuilder:webhook:path=/mutate-korifi-cloudfoundry-org-v1alpha1-all-version,mutating=true,failurePolicy=fail,sideEffects=None,groups=korifi.cloudfoundry.org,resources=cforgs;cfspaces;builderinfos;cfdomains;cfserviceinstances;cfapps;cfpackages;cftasks;cfprocesses;cfbuilds;cfroutes;cfservicebindings;taskworkloads;appworkloads;buildworkloads,verbs=create,versions=v1alpha1,name=mcfversion.korifi.cloudfoundry.org,admissionReviewVersions={v1,v1beta1}
+//+kubebuilder:webhook:path=/mutate-korifi-cloudfoundry-org-v1alpha1-all-version,mutating=true,failurePolicy=fail,sideEffects=None,groups=korifi.cloudfoundry.org,resources=cforgs;cfspaces;builderinfos;cfdomains;cfserviceinstances;cfapps;cfpackages;cftasks;cfprocesses;cfbuilds;cfroutes;cfservicebindings;taskworkloads;appworkloads;buildworkloads,verbs=create;update,versions=v1alpha1,name=mcfversion.korifi.cloudfoundry.org,admissionReviewVersions={v1,v1beta1}
 
 import (
 	"context"
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"code.cloudfoundry.org/korifi/version"
+	corev1 "k8s.io/api/admission/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -45,6 +46,17 @@ func (r *VersionWebhook) Handle(ctx context.Context, req admission.Request) admi
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
+	switch req.Operation {
+	case corev1.Create:
+		return r.setVersion(ctx, obj, r.version)
+	case corev1.Update:
+		return r.resetVersion(ctx, obj, req)
+	default:
+		return admission.Denied("we only accept create/update")
+	}
+}
+
+func (r *VersionWebhook) setVersion(ctx context.Context, obj metav1.PartialObjectMetadata, ver string) admission.Response {
 	origMarshalled, err := json.Marshal(obj)
 	if err != nil {
 		return admission.Errored(http.StatusInternalServerError, err)
@@ -63,4 +75,21 @@ func (r *VersionWebhook) Handle(ctx context.Context, req admission.Request) admi
 	}
 
 	return admission.PatchResponseFromRaw(origMarshalled, marshalled)
+}
+
+func (r *VersionWebhook) resetVersion(ctx context.Context, obj metav1.PartialObjectMetadata, req admission.Request) admission.Response {
+	if _, ok := obj.Annotations[version.KorifiCreationVersionKey]; ok {
+		return admission.Allowed("already set")
+	}
+
+	var oldObj metav1.PartialObjectMetadata
+	if err := r.decoder.DecodeRaw(req.OldObject, &oldObj); err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	if oldVersion, ok := oldObj.Annotations[version.KorifiCreationVersionKey]; ok {
+		return r.setVersion(ctx, obj, oldVersion)
+	}
+
+	return admission.Allowed("no old version")
 }
