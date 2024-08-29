8b7b3c6ee675fd8d276f24911ee67c378bfe0f57
Georgi Sabev <georgethebeatle@gmail.com>
2024-08-29 12:39:51 +0000

    Service broker catalog cli tests
    CLI tests for the following commands:
    * `create-service-broker`
    * `delete-service-broker`
    * `service-access`
    * `enable-service-access`
    * `marketplace`

---
+++ tests/smoke/catalog_test.go
@@ -0,0 +1,116 @@
+package smoke_test
+
+import (
+	"strings"
+
+	"code.cloudfoundry.org/korifi/tests/helpers"
+	"code.cloudfoundry.org/korifi/tests/helpers/broker"
+
+	"github.com/BooleanCat/go-functional/v2/it"
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+	"github.com/onsi/gomega/types"
+)
+
+var _ = Describe("Service Catalog", func() {
+	var brokerName string
+
+	BeforeEach(func() {
+		brokerName = uuid.NewString()
+		Expect(helpers.Cf(
+			"create-service-broker",
+			brokerName,
+			"broker-user",
+			"broker-password",
+			helpers.GetInClusterURL(getAppGUID(brokerAppName)),
+		)).To(Exit(0))
+	})
+
+	AfterEach(func() {
+		cleanupBroker(brokerName)
+	})
+
+	Describe("cf service-brokers", func() {
+		It("lists service brokers", func() {
+			session := helpers.Cf("service-brokers")
+			Expect(session).To(Exit(0))
+
+			lines, _ := it.Collect2(it.LinesString(session.Out))
+			Expect(lines).To(ContainElement(
+				matchSubstrings(brokerName, helpers.GetInClusterURL(getAppGUID(brokerAppName)))))
+		})
+	})
+
+	Describe("cf delete-service-broker", func() {
+		It("deletes the service broker", func() {
+			session := helpers.Cf("delete-service-broker", "-f", brokerName)
+			Expect(session).To(Exit(0))
+
+			session = helpers.Cf("service-brokers")
+			Expect(session).To(Exit(0))
+
+			lines, _ := it.Collect2(it.LinesString(session.Out))
+			Expect(lines).NotTo(ContainElement(ContainSubstring(brokerName)))
+		})
+	})
+
+	Describe("cf service-access", func() {
+		It("lists service access settings", func() {
+			session := helpers.Cf("service-access", "-b", brokerName)
+			Expect(session).To(Exit(0))
+
+			lines, _ := it.Collect2(it.LinesString(session.Out))
+			Expect(lines).To(ContainElements(
+				matchSubstrings("sample-service", "sample", "none"),
+			))
+		})
+	})
+
+	Describe("cf enable-service-access", func() {
+		It("enables the service access", func() {
+			session := helpers.Cf("enable-service-access", "sample-service", "-b", brokerName)
+			Expect(session).To(Exit(0))
+
+			session = helpers.Cf("service-access")
+			Expect(session).To(Exit(0))
+
+			lines, _ := it.Collect2(it.LinesString(session.Out))
+			Expect(lines).To(ContainElements(
+				matchSubstrings("sample-service", "sample", "all"),
+			))
+		})
+	})
+
+	Describe("cf marketplace", func() {
+		It("lists the service catalog", func() {
+			session := helpers.Cf("marketplace", "-b", brokerName)
+			Expect(session).To(Exit(0))
+
+			lines, _ := it.Collect2(it.LinesString(session.Out))
+			Expect(lines).To(ContainElement(
+				matchSubstrings("sample-service", "A sample service that does nothing", brokerName),
+			))
+		})
+	})
+})
+
+func getAppGUID(appName string) string {
+	GinkgoHelper()
+
+	session := helpers.Cf("app", appName, "--guid")
+	Expect(session).To(Exit(0))
+	return string(session.Out.Contents())
+}
+
+func cleanupBroker(brokerName string) {
+	GinkgoHelper()
+
+	Expect(helpers.Cf("delete-service-broker", "-f", brokerName)).To(Exit(0))
+	broker.NewCatalogPurger(rootNamespace).ForBrokerName(brokerName).Purge()
+}
+
+func matchSubstrings(substrings ...string) types.GomegaMatcher {
+	return MatchRegexp(strings.Join(substrings, ".*"))
+}
