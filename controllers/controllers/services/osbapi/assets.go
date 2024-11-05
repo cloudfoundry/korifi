d75d43058d0de737f0b7a1eb75353a1bb488596f
Yusmen Zabanov <yusmen.zabanov@sap.com>
2024-11-05 11:07:06 +0000

    Use `GetServiceInstanceAssets` in managed services controller
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- controllers/controllers/services/osbapi/assets.go
+++ controllers/controllers/services/osbapi/assets.go
@@ -21,50 +21,6 @@ func NewAssets(k8sClient client.Client, rootNamespace string) *Assets {
 	}
 }
 
-func (r *Assets) GetServiceOffering(ctx context.Context, offeringGUID string) (*korifiv1alpha1.CFServiceOffering, error) {
-	serviceOffering := &korifiv1alpha1.CFServiceOffering{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      offeringGUID,
-			Namespace: r.rootNamespace,
-		},
-	}
-	err := r.k8sClient.Get(ctx, client.ObjectKeyFromObject(serviceOffering), serviceOffering)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get service offering %q: %w", offeringGUID, err)
-	}
-
-	return serviceOffering, nil
-}
-
-func (r *Assets) GetServicePlan(ctx context.Context, planGUID string) (*korifiv1alpha1.CFServicePlan, error) {
-	servicePlan := &korifiv1alpha1.CFServicePlan{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      planGUID,
-			Namespace: r.rootNamespace,
-		},
-	}
-	err := r.k8sClient.Get(ctx, client.ObjectKeyFromObject(servicePlan), servicePlan)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get service plan %q: %w", planGUID, err)
-	}
-	return servicePlan, nil
-}
-
-func (r *Assets) GetServiceBroker(ctx context.Context, brokerGUID string) (*korifiv1alpha1.CFServiceBroker, error) {
-	serviceBroker := &korifiv1alpha1.CFServiceBroker{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      brokerGUID,
-			Namespace: r.rootNamespace,
-		},
-	}
-	err := r.k8sClient.Get(ctx, client.ObjectKeyFromObject(serviceBroker), serviceBroker)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get service broker %q: %w", brokerGUID, err)
-	}
-
-	return serviceBroker, nil
-}
-
 type ServiceInstanceAssets struct {
 	ServiceBroker   *korifiv1alpha1.CFServiceBroker
 	ServiceOffering *korifiv1alpha1.CFServiceOffering
@@ -72,17 +28,17 @@ type ServiceInstanceAssets struct {
 }
 
 func (r *Assets) GetServiceInstanceAssets(ctx context.Context, serviceInstance *korifiv1alpha1.CFServiceInstance) (ServiceInstanceAssets, error) {
-	servicePlan, err := r.GetServicePlan(ctx, serviceInstance.Spec.PlanGUID)
+	servicePlan, err := r.getServicePlan(ctx, serviceInstance.Spec.PlanGUID)
 	if err != nil {
 		return ServiceInstanceAssets{}, err
 	}
 
-	serviceBroker, err := r.GetServiceBroker(ctx, servicePlan.Labels[korifiv1alpha1.RelServiceBrokerGUIDLabel])
+	serviceBroker, err := r.getServiceBroker(ctx, servicePlan.Labels[korifiv1alpha1.RelServiceBrokerGUIDLabel])
 	if err != nil {
 		return ServiceInstanceAssets{}, err
 	}
 
-	serviceOffering, err := r.GetServiceOffering(ctx, servicePlan.Labels[korifiv1alpha1.RelServiceOfferingGUIDLabel])
+	serviceOffering, err := r.getServiceOffering(ctx, servicePlan.Labels[korifiv1alpha1.RelServiceOfferingGUIDLabel])
 	if err != nil {
 		return ServiceInstanceAssets{}, err
 	}
@@ -120,4 +76,48 @@ func (r *Assets) GetServiceBindingAssets(ctx context.Context, serviceBinding *ko
 		ServiceInstanceAssets: serviceInstanceAssets,
 		ServiceInstance:       serviceInstance,
 	}, nil
+}
+
+func (r *Assets) getServiceOffering(ctx context.Context, offeringGUID string) (*korifiv1alpha1.CFServiceOffering, error) {
+	serviceOffering := &korifiv1alpha1.CFServiceOffering{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      offeringGUID,
+			Namespace: r.rootNamespace,
+		},
+	}
+	err := r.k8sClient.Get(ctx, client.ObjectKeyFromObject(serviceOffering), serviceOffering)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get service offering %q: %w", offeringGUID, err)
+	}
+
+	return serviceOffering, nil
+}
+
+func (r *Assets) getServicePlan(ctx context.Context, planGUID string) (*korifiv1alpha1.CFServicePlan, error) {
+	servicePlan := &korifiv1alpha1.CFServicePlan{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      planGUID,
+			Namespace: r.rootNamespace,
+		},
+	}
+	err := r.k8sClient.Get(ctx, client.ObjectKeyFromObject(servicePlan), servicePlan)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get service plan %q: %w", planGUID, err)
+	}
+	return servicePlan, nil
+}
+
+func (r *Assets) getServiceBroker(ctx context.Context, brokerGUID string) (*korifiv1alpha1.CFServiceBroker, error) {
+	serviceBroker := &korifiv1alpha1.CFServiceBroker{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      brokerGUID,
+			Namespace: r.rootNamespace,
+		},
+	}
+	err := r.k8sClient.Get(ctx, client.ObjectKeyFromObject(serviceBroker), serviceBroker)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get service broker %q: %w", brokerGUID, err)
+	}
+
+	return serviceBroker, nil
 }
