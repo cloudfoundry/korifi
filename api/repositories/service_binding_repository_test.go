8ff4258929c306497e6117b14ebed09bd36f1f0f
Dimitar Draganov <dimitar.draganov@sap.com>
2024-11-26 10:24:49 +0200

    Fixed build errors related to a recent name change

--- api/repositories/service_binding_repository_test.go
+++ api/repositories/service_binding_repository_test.go
@@ -551,7 +551,7 @@ var _ = Describe("ServiceBindingRepo", func() {
 					Expect(
 						k8sClient.Get(ctx, types.NamespacedName{Name: serviceBindingRecord.GUID, Namespace: space.Name}, serviceBinding),
 					).To(Succeed())
-
+					korifiv1alpha1.SchemeGroupVersion.Identifier()
 					Expect(serviceBinding.Labels).To(HaveKeyWithValue("servicebinding.io/provisioned-service", "true"))
 					Expect(serviceBinding.Spec.Type).To(Equal(korifiv1alpha1.CFServiceBindingTypeKey))
 					Expect(serviceBinding.Spec).To(Equal(
@@ -559,7 +559,7 @@ var _ = Describe("ServiceBindingRepo", func() {
 							DisplayName: nil,
 							Service: corev1.ObjectReference{
 								Kind:       "CFServiceInstance",
-								APIVersion: korifiv1alpha1.GroupVersion.Identifier(),
+								APIVersion: korifiv1alpha1.SchemeGroupVersion.Identifier(),
 								Name:       cfServiceInstance.Name,
 							},
 						},
@@ -631,7 +631,7 @@ var _ = Describe("ServiceBindingRepo", func() {
 							DisplayName: nil,
 							Service: corev1.ObjectReference{
 								Kind:       "CFServiceInstance",
-								APIVersion: korifiv1alpha1.GroupVersion.Identifier(),
+								APIVersion: korifiv1alpha1.SchemeGroupVersion.Identifier(),
 								Name:       cfServiceInstance.Name,
 							},
 							AppRef: corev1.LocalObjectReference{
