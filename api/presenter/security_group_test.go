30d277718b4ef6b3d07c264575db65c95cea229f
Konstantin Lapkov <k.lapkov@sap.com>
2025-06-05 14:32:50 +0300

    Introduce Controller for Security Groups and bind, get endpoints

--- api/presenter/security_group_test.go
+++ api/presenter/security_group_test.go
@@ -45,61 +45,114 @@ var _ = Describe("SecurityGroup", func() {
 		}
 	})
 
-	JustBeforeEach(func() {
-		response := presenter.ForSecurityGroup(record, *baseURL)
-		var err error
-		output, err = json.Marshal(response)
-		Expect(err).NotTo(HaveOccurred())
-	})
-
-	It("returns the expected JSON", func() {
-		Expect(output).To(MatchJSON(`{
-            "guid": "security-group-guid",
-            "created_at": "1970-01-01T00:00:01Z",
-            "updated_at": "1970-01-01T00:00:02Z",
-            "name": "my-security-group",
-            "globally_enabled": {
-                "running": true,
-                "staging": false
-            },
-            "rules": [
-                {
-                    "protocol": "tcp",
-                    "ports": "80",
-                    "destination": "192.168.1.1"
-                }
-            ],
-            "relationships": {
-                "running_spaces": {
-                    "data": [
-                        { "guid": "space-1" },
-                        { "guid": "space-2" }
-                    ]
-                },
-                "staging_spaces": {
-                    "data": [
-                        { "guid": "space-3" }
-                    ]
-                }
-            },
-            "links": {
-                "self": {
-                    "href": "https://api.example.org/v3/security_groups/security-group-guid"
-                }
-            }
-        }`))
-	})
-
-	When("there are no rules and no spaces", func() {
-		BeforeEach(func() {
-			record.Rules = []repositories.SecurityGroupRule{}
-			record.RunningSpaces = []string{}
-			record.StagingSpaces = []string{}
+	Describe("ForSecurityGroup", func() {
+		JustBeforeEach(func() {
+			response := presenter.ForSecurityGroup(record, *baseURL)
+			var err error
+			output, err = json.Marshal(response)
+			Expect(err).NotTo(HaveOccurred())
 		})
 
-		It("returns empty arrays for rules and relationships", func() {
-			Expect(output).To(MatchJSONPath("$.relationships.running_spaces.data", BeEmpty()))
-			Expect(output).To(MatchJSONPath("$.relationships.staging_spaces.data", BeEmpty()))
+		It("returns the expected JSON", func() {
+			Expect(output).To(MatchJSON(`{
+				"guid": "security-group-guid",
+				"created_at": "1970-01-01T00:00:01Z",
+				"updated_at": "1970-01-01T00:00:02Z",
+				"name": "my-security-group",
+				"globally_enabled": {
+					"running": true,
+					"staging": false
+				},
+				"rules": [
+					{
+						"protocol": "tcp",
+						"ports": "80",
+						"destination": "192.168.1.1"
+					}
+				],
+				"relationships": {
+					"running_spaces": {
+						"data": [
+							{ "guid": "space-1" },
+							{ "guid": "space-2" }
+						]
+					},
+					"staging_spaces": {
+						"data": [
+							{ "guid": "space-3" }
+						]
+					}
+				},
+				"links": {
+					"self": {
+						"href": "https://api.example.org/v3/security_groups/security-group-guid"
+					}
+				}
+			}`))
+		})
+
+		When("there are no rules and no spaces", func() {
+			BeforeEach(func() {
+				record.Rules = []repositories.SecurityGroupRule{}
+				record.RunningSpaces = []string{}
+				record.StagingSpaces = []string{}
+			})
+
+			It("returns empty arrays for rules and relationships", func() {
+				Expect(output).To(MatchJSONPath("$.relationships.running_spaces.data", BeEmpty()))
+				Expect(output).To(MatchJSONPath("$.relationships.staging_spaces.data", BeEmpty()))
+			})
+		})
+	})
+
+	Describe("ForSecurityGroupStagingSpaces", func() {
+		JustBeforeEach(func() {
+			response := presenter.ForSecurityGroupRunningSpaces(record, *baseURL)
+			var err error
+			output, err = json.Marshal(response)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("returns the expected JSON", func() {
+			Expect(output).To(MatchJSON(`{
+				"data": [
+    				{ 
+						"guid": "space-1"
+					},
+					{
+				 		"guid": "space-2"
+					}
+				],
+  				"links": {
+    			"self": {
+      				"href": "https://api.example.org/v3/security_groups/security-group-guid/relationships/running_spaces"
+    				}
+  				}
+			}`))
+		})
+	})
+
+	Describe("ForSecurityGroupStagingSpaces", func() {
+		JustBeforeEach(func() {
+			response := presenter.ForSecurityGroupStagingSpaces(record, *baseURL)
+			var err error
+			output, err = json.Marshal(response)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("returns the expected JSON", func() {
+			Expect(output).To(MatchJSON(`{
+				"data": [
+    				{ 
+						"guid": "space-3" 
+					}
+				],
+  				"links": {
+    			"self": {
+      				"href": "https://api.example.org/v3/security_groups/security-group-guid/relationships/staging_spaces"
+    				}
+  				}
+			}`))
 		})
 	})
 })
--- api/repositories/security_group_repository_test.go
+++ api/repositories/security_group_repository_test.go
@@ -1,10 +1,13 @@
 package repositories_test
 
 import (
+	"errors"
+
 	apierrors "code.cloudfoundry.org/korifi/api/errors"
 	"code.cloudfoundry.org/korifi/api/repositories"
 	korifiv1alpha1 "code.cloudfoundry.org/korifi/controllers/api/v1alpha1"
 	"code.cloudfoundry.org/korifi/tests/matchers"
+	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -22,6 +25,81 @@ var _ = Describe("SecurityGroupRepo", func() {
 		repo = repositories.NewSecurityGroupRepo(klient, rootNamespace)
 		org = createOrgWithCleanup(ctx, prefixedGUID("org"))
 		space = createSpaceWithCleanup(ctx, org.Name, prefixedGUID("space"))
+	})
+
+	Describe("GetSecurityGroup", func() {
+		var (
+			securityGroup       *korifiv1alpha1.CFSecurityGroup
+			securityGroupRecord repositories.SecurityGroupRecord
+			securityGroupGUID   string
+			getErr              error
+		)
+
+		BeforeEach(func() {
+			securityGroupGUID = uuid.NewString()
+
+			securityGroup = &korifiv1alpha1.CFSecurityGroup{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      securityGroupGUID,
+					Namespace: rootNamespace,
+				},
+				Spec: korifiv1alpha1.CFSecurityGroupSpec{
+					DisplayName: "test-security-group",
+					Rules: []korifiv1alpha1.SecurityGroupRule{
+						{
+							Protocol:    korifiv1alpha1.ProtocolTCP,
+							Ports:       "80",
+							Destination: "192.168.1.1",
+						},
+					},
+					Spaces: map[string]korifiv1alpha1.SecurityGroupWorkloads{
+						space.Name: {Running: true, Staging: true},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, securityGroup)).To(Succeed())
+		})
+
+		JustBeforeEach(func() {
+			securityGroupRecord, getErr = repo.GetSecurityGroup(ctx, authInfo, securityGroupGUID)
+		})
+
+		It("errors with forbidden for users with no permissions", func() {
+			Expect(getErr).To(matchers.WrapErrorAssignableToTypeOf(apierrors.ForbiddenError{}))
+		})
+
+		When("the user is a CF admin", func() {
+			BeforeEach(func() {
+				createRoleBinding(ctx, userName, adminRole.Name, rootNamespace)
+			})
+
+			It("returns a security group record", func() {
+				Expect(getErr).ToNot(HaveOccurred())
+
+				Expect(securityGroupRecord.GUID).To(Equal(securityGroupGUID))
+				Expect(securityGroupRecord.Name).To(Equal("test-security-group"))
+				Expect(securityGroupRecord.GloballyEnabled.Running).To(BeFalse())
+				Expect(securityGroupRecord.GloballyEnabled.Staging).To(BeFalse())
+				Expect(securityGroupRecord.RunningSpaces).To(ConsistOf(space.Name))
+				Expect(securityGroupRecord.StagingSpaces).To(ConsistOf(space.Name))
+				Expect(securityGroupRecord.Rules).To(ConsistOf(repositories.SecurityGroupRule{
+					Protocol:    korifiv1alpha1.ProtocolTCP,
+					Ports:       "80",
+					Destination: "192.168.1.1",
+				}))
+			})
+
+			When("the security group does not exist", func() {
+				BeforeEach(func() {
+					securityGroupGUID = "does-not-exist"
+				})
+
+				It("returns a not found error", func() {
+					notFoundErr := apierrors.NotFoundError{}
+					Expect(errors.As(getErr, &notFoundErr)).To(BeTrue())
+				})
+			})
+		})
 	})
 
 	Describe("CreateSecurityGroup", func() {
@@ -98,6 +176,88 @@ var _ = Describe("SecurityGroupRepo", func() {
 					Ports:       "80",
 					Destination: "192.168.1.1",
 				}))
+			})
+		})
+	})
+
+	Describe("BindSecurityGroup", func() {
+		var (
+			securityGroup       *korifiv1alpha1.CFSecurityGroup
+			securityGroupRecord repositories.SecurityGroupRecord
+			bindMessage         repositories.BindSecurityGroupMessage
+			bindErr             error
+		)
+
+		BeforeEach(func() {
+			securityGroup = &korifiv1alpha1.CFSecurityGroup{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      uuid.NewString(),
+					Namespace: rootNamespace,
+				},
+				Spec: korifiv1alpha1.CFSecurityGroupSpec{
+					DisplayName: "test-security-group",
+					Rules: []korifiv1alpha1.SecurityGroupRule{
+						{
+							Protocol:    korifiv1alpha1.ProtocolTCP,
+							Ports:       "80",
+							Destination: "192.168.1.1",
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, securityGroup)).To(Succeed())
+
+			bindMessage = repositories.BindSecurityGroupMessage{
+				GUID:     securityGroup.Name,
+				Spaces:   []string{space.Name},
+				Workload: repositories.SecurityGroupRunningSpaceType,
+			}
+		})
+
+		JustBeforeEach(func() {
+			securityGroupRecord, bindErr = repo.BindSecurityGroup(ctx, authInfo, bindMessage)
+		})
+
+		It("errors with forbidden for users with no permissions", func() {
+			Expect(bindErr).To(matchers.WrapErrorAssignableToTypeOf(apierrors.ForbiddenError{}))
+		})
+
+		When("the user is a CF admin", func() {
+			BeforeEach(func() {
+				createRoleBinding(ctx, userName, adminRole.Name, rootNamespace)
+			})
+
+			It("binds the space to the cfSecurityGroup", func() {
+				Expect(bindErr).ToNot(HaveOccurred())
+
+				cfSecurityGroup := &korifiv1alpha1.CFSecurityGroup{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      securityGroup.Name,
+						Namespace: rootNamespace,
+					},
+				}
+
+				Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(cfSecurityGroup), cfSecurityGroup)).To(Succeed())
+				Expect(cfSecurityGroup.Spec.Spaces).To(Equal(map[string]korifiv1alpha1.SecurityGroupWorkloads{
+					space.Name: {Running: true, Staging: false},
+				}))
+			})
+
+			It("returns the correct record", func() {
+				Expect(bindErr).ToNot(HaveOccurred())
+				Expect(securityGroupRecord.RunningSpaces).To(ConsistOf([]string{space.Name}))
+				Expect(securityGroupRecord.StagingSpaces).To(BeEmpty())
+			})
+
+			When("the security group does not exist", func() {
+				BeforeEach(func() {
+					bindMessage.GUID = "does-not-ecist"
+				})
+
+				It("returns a not found error", func() {
+					notFoundErr := apierrors.NotFoundError{}
+					Expect(errors.As(bindErr, &notFoundErr)).To(BeTrue())
+				})
 			})
 		})
 	})
