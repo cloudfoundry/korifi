b895621a23f5c1cc81fef49b40ca805aed54b966
BG <bguttmann@anynines.com>
2025-12-12 11:14:18 +0100

    Remove not needed org_quota name

--- tests/smoke/org_quota_test.go
+++ tests/smoke/org_quota_test.go
@@ -3,18 +3,12 @@ package smoke_test
 import (
 	"code.cloudfoundry.org/korifi/tests/helpers"
 
-	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("OrgQuotas", func() {
-	var orgQuotaName string
-	BeforeEach(func() {
-		orgQuotaName = uuid.NewString()
-	})
-
 	Describe("cf org-quota", func() {
 		It("returns OK", func() {
 			session := helpers.Cf("org-quotas")
